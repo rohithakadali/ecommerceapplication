98c8c6a6dfd3194dae0ff542e0e11f0d3b3bdcf6
rohithakadali <rohithakadali0@gmail.com>
2023-09-22 10:38:47 +0530

    adding ecommerce application to online repo

--- category/models.py
+++ category/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Category(models.Model):
     category_name=models.CharField(max_length=50,unique=True)
     slug=models.SlugField(max_length=100,unique=True)
-    descrption=models.TextField(max_length=225,blank=True)
+    description=models.TextField(max_length=225,blank=True)
     cat_image=models.ImageField(upload_to='photos/categories',blank=True)
     
     class Meta:
--- store/models.py
+++ store/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from category.models import Category
+from django.urls import reverse
+
 class Product(models.Model):
     product_name=models.CharField(max_length=200,unique=True)
     slug=models.SlugField(max_length=200,unique=True)
-    descrption=models.TextField(max_length=500,blank=True)
+    description=models.TextField(max_length=500,blank=True)
     price=models.IntegerField()
     images=models.ImageField(upload_to='photos/products')
     stock=models.IntegerField()
@@ -11,6 +13,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date=models.DateTimeField(auto_now_add=True)
     modified_date=models.DateField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail',args=[self.category.slug,self.slug])
     
     def __str__(self):
         return self.product_name
